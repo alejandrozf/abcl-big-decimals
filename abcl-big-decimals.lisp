2b87e66038877c8d2699ce62d60334d09b70671f
alejandrozf <ale2014.zamora@gmail.com>
2021-12-30 14:21:12 -0300

    - Some refactor for avoid duplication

--- abcl-big-decimals.lisp
+++ abcl-big-decimals.lisp
@@ -6,37 +6,22 @@
   "BIG-DECIMAL-DESIGNATOR can be a real or a string"
   (java:jnew "java.math.BigDecimal" big-decimal-designator))
 
-(defun divide (big-decimal-1 big-decimal-2)
-  "Divide two BIG-DECIMAL objects"
-  ;; TODO: precision should be a parameter
-  (let ((jmethod-instance (java:jmethod "java.math.BigDecimal" "divide"
-                                        "java.math.BigDecimal" "java.math.MathContext"))
-        (precision (java:jfield "java.math.MathContext" "DECIMAL128")))
-    (java:jcall jmethod-instance big-decimal-1 big-decimal-2 precision)))
 
-(defun add (big-decimal-1 big-decimal-2)
-  "Add two BIG-DECIMAL objects"
-  ;; TODO: precision should be a parameter
-  (let ((jmethod-instance (java:jmethod "java.math.BigDecimal" "add"
-                                        "java.math.BigDecimal" "java.math.MathContext"))
-        (precision (java:jfield "java.math.MathContext" "DECIMAL128")))
-    (java:jcall jmethod-instance big-decimal-1 big-decimal-2 precision)))
+(macrolet ((defoperator (name)
+             `(defun ,name (big-decimal-1 big-decimal-2)
+                ,(format nil "~a two BIG-DECIMAL objects" name)
+                ;; TODO: precision should be a parameter
+                (let ((jmethod-instance (java:jmethod "java.math.BigDecimal"
+                                                      ,(string-downcase (write-to-string name))
+                                                      "java.math.BigDecimal" "java.math.MathContext"))
+                      (precision (java:jfield "java.math.MathContext" "DECIMAL128")))
+                  (java:jcall jmethod-instance big-decimal-1 big-decimal-2 precision)))))
 
-(defun multiply (big-decimal-1 big-decimal-2)
-  "Multiply two BIG-DECIMAL objects"
-  ;; TODO: precision should be a parameter
-  (let ((jmethod-instance (java:jmethod "java.math.BigDecimal" "multiply"
-                                        "java.math.BigDecimal" "java.math.MathContext"))
-        (precision (java:jfield "java.math.MathContext" "DECIMAL128")))
-    (java:jcall jmethod-instance big-decimal-1 big-decimal-2 precision)))
+  (defoperator divide)
+  (defoperator add)
+  (defoperator multiply)
+  (defoperator subtract))
 
-(defun subtract (big-decimal-1 big-decimal-2)
-  "Subtract two BIG-DECIMAL objects"
-  ;; TODO: precision should be a parameter
-  (let ((jmethod-instance (java:jmethod "java.math.BigDecimal" "subtract"
-                                        "java.math.BigDecimal" "java.math.MathContext"))
-        (precision (java:jfield "java.math.MathContext" "DECIMAL128")))
-    (java:jcall jmethod-instance big-decimal-1 big-decimal-2 precision)))
 
 (defun big-decimal->string (big-decimal)
   "Convert BIG-DECIMAL to a CL string (can use scientific notation if needed)"
