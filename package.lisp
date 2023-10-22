c1596f1dc25868ca11f42b0b7c1ad4eebd93e5f9
Alejandro Zamora <ale2014.zamora@gmail.com>
2023-10-21 23:15:11 -0300

    little fix

--- package.lisp
+++ package.lisp
@@ -2,14 +2,18 @@
 
 (defpackage #:abcl-big-decimals
   (:use #:cl)
+  (:shadow #:>)
   (:export
-   :make-big-decimal
-   :divide
-   :add
-   :multiply
-   :subtract
-   :pow
-   :big-decimal->string
-   :big-decimal->plain-string
-   :big-decimal->double-float)
-  (:shadow #:>))
+   #:make-big-decimal
+   #:divide
+   #:add
+   #:multiply
+   #:subtract
+   #:pow
+   #:big-decimal->string
+   #:big-decimal->plain-string
+   #:big-decimal->double-float))
+
+(in-package :abcl-big-decimals)
+
+(export '>)
